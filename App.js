c9c06746639e014b070d42d8501bfda46f78cf63
Dana Simmons <dana@Danas-MacBook-Pro.local>
2019-05-27 11:08:35 -0700

    started the thing, app renders with navigation container

---
+++ App.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation'
+
+type Props = {};
+class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
+}
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen : App
+  }
+})
+
+export default createAppContainer(AppNavigator)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ README.md
@@ -0,0 +1,12 @@
+# Pocket Venue React Native
+
+## Getting Started
+### [Please Use Yarn] Yarn package manager will save you a lot of headaches
+### Setup
+1. `yarn install`
+2. `react-native link react-native-gesture-handler`
+3. `react-native link react-native-tcp`
+4. `./node_modules/.bin/rn-nodeify --install --hack`
+5. `react-native run-ios` 
+
+This should get you up and running
