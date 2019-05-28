90e504d877723912674d3f16a23ff2b478cc941a
Dana Simmons <dana@simmons-audio.com>
2019-05-27 22:57:29 -0400

    started styling list view and added button toolbar

--- App.js
+++ App.js
@@ -7,9 +7,8 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, FlatList} from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation'
-import VenueItem from './src/VenueItem'
+import {Platform, StyleSheet, Text, View, FlatList, Button} from 'react-native';
 
 
 class App extends Component {
@@ -19,21 +18,24 @@ class App extends Component {
       venues: [
         {key: 'a', name: "Venue 1", address: "127.0.0.1", port: 4080},
         {key: 'b', name: "Venue 2", address: "127.0.0.2", port: 4080},
-	{key: 'c', name: "Venue 3", address: "" , port: 0}
+        {key: 'c', name: "Venue 3", address: "" , port: 0}
       ]
     }
   }
   render() {
     return (
-	<View style={{flex:1}}>
-	<FlatList
-	style={{paddingTop:100, flex:1}} 
-        data={this.state.venues}
-        renderItem={({item})=><Text>{item.name}</Text>} />
+	<View style={{flex: 1}}>
+    <View style={styles.toolbar}>
+      <Button title={"New Venue"} />
+      <Button title={"Edit"}/>
+    </View>
+    <FlatList
+    contentContainerStyle={styles.listView}
+    data={this.state.venues}
+    renderItem={({item})=><Text style={styles.listItem}>{item.name}</Text>} />
 	</View>
 
-    );
-  }
+    )}
 }
 
 const AppNavigator = createStackNavigator({
@@ -41,6 +43,22 @@ const AppNavigator = createStackNavigator({
     screen : App
   }
 })
-
 export default createAppContainer(AppNavigator)
+
+const styles = StyleSheet.create({
+  toolbar: {
+    fontSize: 22,
+    backgroundColor: '#CCC',
+    flexDirection: 'row'
+  },
+  listView:{
+    flexDirection: 'column',
+    backgroundColor: '#EEE',
+    borderWidth: 1
+  },
+  listItem:{
+    borderWidth: 1,
+    fontSize: 22
+  }
+})
 
