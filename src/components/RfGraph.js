5868422cd978d6d7ea69406b8c83dd0893b5ea6d
Dana Simmons <dsanalog@gmail.com>
2019-06-13 21:27:09 -0400

    tweaking the graph scale. For some reason the graph x axis doesn't allign properly with the display data. Also changed the iOS project device settings to be 'universal' platform for iphone and iPad

--- src/components/RfGraph.js
+++ src/components/RfGraph.js
@@ -1,23 +1,45 @@
 import React, { Component } from 'react'
 import {View, Text} from 'react-native';
-import {AreaChart, LineChart, Grid} from 'react-native-svg-charts';
-
+import {AreaChart, LineChart, Grid, XAxis} from 'react-native-svg-charts';
+import PropTypes from 'prop-types'
+import * as scale from 'd3-scale'
+const stepSize = .1
 export default class RfGraph extends Component {
+    
     render() {
         const { scan } = this.props
         return (
             <View>
+            <Text>Block: {scan.block} {scan.start.toFixed(2)}MHz - {scan.end.toFixed(2)}MHz </Text>
                 <AreaChart
-                    style={{ height:200 }}
+                    style={{ height:150 }}
                     data={ scan.scan }
+                    scale = {scale.scaleLinear}
+                    xAccessor = { ( {index} ) => scan.start + (index * stepSize) }
                     svg={{ 
                         fill: 'rgba(255, 114, 0, 0.5)',
                         stroke: 'rgba(255, 114, 0, 1)' }}
                     contentInset={{ top:20, bottom: 20}}>
                         <Grid />
                 </AreaChart>
-                <Text>Block: {scan.block} Start: {scan.start.toFixed(2)} End: {scan.end.toFixed(2)} </Text>
+                <XAxis 
+                    data = {scan.scan}
+                    style={{}}
+                    contentInset = {{left: 20, right: 20}}
+                    scale={scale.scaleLinear}
+                    numberOfTicks={12}
+                    formatLabel={ (value, index) => (scan.start + (index * stepSize)).toFixed(2)}
+                    />
             </View>
         )
     }
+}
+
+RfGraph.propTypes = {
+    scan: PropTypes.shape({
+        scan: PropTypes.array.isRequired,
+        start: PropTypes.number.isRequired,
+        end: PropTypes.number.isRequired,
+        block: PropTypes.string
+    })
 }
