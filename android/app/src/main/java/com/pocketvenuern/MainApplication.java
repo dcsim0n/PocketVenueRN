3bcbe5671d8f4d0ad44a175e5b6952b78ae02522
Dana Simmons <dsanalog@gmail.com>
2019-06-14 22:28:30 -0400

    experimenting with zoom and pan

--- android/app/src/main/java/com/pocketvenuern/MainApplication.java
+++ android/app/src/main/java/com/pocketvenuern/MainApplication.java
@@ -3,6 +3,7 @@ package com.pocketvenuern;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.slider.ReactSliderPackage;
 import com.horcrux.svg.SvgPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.peel.react.TcpSocketsModule;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactSliderPackage(),
             new SvgPackage(),
             new RNGestureHandlerPackage(),
             new TcpSocketsModule()
