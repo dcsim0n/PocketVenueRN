c49711464faba42616112c7f10d8cd6c2c78bf2a
Dana Simmons <dsanalog@gmail.com>
2019-06-16 20:37:06 -0400

    Add edit UI for transmitters
    begining to create UI to edit Transmitter properties.
    Text labels and frequency picker

--- src/components/DataDetails.js
+++ src/components/DataDetails.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Picker } from 'react-native'
 
 const DataDetails = (props) => {
+    const {label,frequency,voltage,block} = props.navigation.getParam('item')
     return (
         <View>
-            <Text>Hello World</Text>
+            <Text>Label: {label}</Text>
+            <Text>Block: {block}</Text>
+            <Text>Voltage: {voltage}</Text>
+            <Picker
+                selectedValue={frequency}
+                style={{height: 50, width: 100}}
+                onValueChange={(itemValue, itemIndex) =>
+                    this.setState({language: itemValue})
+            }>
+                <Picker.Item label={frequency} value={frequency} />
+            </Picker>
         </View>
     )
 }
