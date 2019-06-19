afce616e0f14d8b8e1f4e85d0d0d4a74f21a4c4b
Dana Simmons <dsanalog@gmail.com>
2019-06-18 20:46:24 -0400

    Populate frequency picker
    calculate frequency ranges and populate the picker with them.

--- src/components/DataDetails.js
+++ src/components/DataDetails.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import { View, Text, Picker } from 'react-native'
 
+const blocks = require('../lib/blocks.json')
+
 const DataDetails = (props) => {
     const {label,frequency,voltage,block} = props.navigation.getParam('item')
+    
+    const freqs = []
+    for(let freq = blocks[block].start; freq <= blocks[block].end; freq += .1){
+          freqs.push(parseFloat(freq.toFixed(2))) //Avoid javascript floating point addition errors      
+    }
     return (
         <View>
             <Text>Label: {label}</Text>
             <Text>Block: {block}</Text>
+            <Text>Frequency: {frequency}</Text>
             <Text>Voltage: {voltage}</Text>
             <Picker
                 selectedValue={frequency}
                 style={{height: 50, width: 100}}
                 onValueChange={(itemValue, itemIndex) =>
-                    this.setState({language: itemValue})
+                    console.log(itemValue)
             }>
-                <Picker.Item label={frequency} value={frequency} />
+                {freqs.map(( freq ) => <Picker.Item label={ freq.toFixed(2) } value={ freq } />)}
             </Picker>
         </View>
     )
