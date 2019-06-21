bdacc8be63917064f9a105c1abd7d4086af20642
Dana Simmons <dsanalog@gmail.com>
2019-06-21 12:16:39 -0400

    Create hooks for DataDetails state
    implemented a hook based state for the DataDetails component.
    Pickers and Sliders are now connected to a state variable.
    the state data can be sent to the remote device eventually

--- src/components/DataDetails.js
+++ src/components/DataDetails.js
@@ -1,63 +1,88 @@
-import React from 'react'
-import { View, Text, Picker, TextInput, Button } from 'react-native'
-import Slider from '@react-native-community/slider'
-import styles from '../stylesheets/appStyles'
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { View, Text, Picker, TextInput, Button } from "react-native";
+import Slider from "@react-native-community/slider";
+import styles from "../stylesheets/appStyles";
 
-const blocks = require('../lib/blocks.json')
+const blocks = require("../lib/blocks.json");
 
+const DataDetails = ( props ) => {
 
+  const item = props.navigation.getParam("item");
+  const [frequency, changeFrequency] = useState(item.frequency);
+  const [batteryType, changeBattery] = useState(item.batteryType);
+  const [outLevel, changeLevel] = useState(item.outLevel);
 
-const DataDetails = (props) => {
-    const {label,frequency,voltage, block, batteryType, outLevel, index} = props.navigation.getParam('item')
-    const {BatteryTypes} = props.navigation.getParam('device')
-    const freqs = []
-    for(let freq = blocks[block].start; freq <= blocks[block].end; freq += .1){
-          freqs.push(parseFloat(freq.toFixed(2))) //Avoid javascript floating point addition errors      
-    }
-    return (
-        <View style={styles.container}>
-                <Text style={styles.baseText}>Ch: {index}, Label: </Text>
-                <TextInput 
-                    value={ label }
-                    style={[styles.textInput, {textAlign: 'center'}]}
-                />
-                <Text style={styles.baseText}>Block: {block}</Text>
-                <Text style={styles.baseText}>Voltage: {voltage} V</Text>
-            
-            
-            <View style={{ flex: 1, flexDirection: 'row'}}>
-                <Picker
-                    selectedValue={frequency}
-                    style={{height: 50, flex:1 }}
-                    onValueChange={(itemValue, itemIndex) =>
-                        console.log(itemValue)
-                }>
-                    {freqs.map(( freq ) => <Picker.Item key={ freq } label={ freq.toFixed(2) + ' MHz' } value={ freq } />)}
-                </Picker>
+  const { BatteryTypes } = props.navigation.getParam("device");
 
-                <Picker 
-                    selectedValue={batteryType}
-                    style={{height: 50, flex: 1}}
-                    onValueChange={() => console.log("TODO: Change Battery Type")}
-                >
-                    {Object.keys(BatteryTypes).map(( key ) => <Picker.Item key={ key } label={ BatteryTypes[key] } value={ key } />)} 
-                </Picker>
-            </View>
+  const freqs = [];
+  for (let freq = blocks[item.block].start; freq <= blocks[item.block].end; freq += 0.1) {
+    freqs.push(parseFloat(freq.toFixed(2))); //Avoid javascript floating point addition errors
+  }
 
-            <View style={styles.container}> 
-                <Text>Channel Output Level: {/* Output Level */} </Text>
-                <Slider 
-                    width={ 200 } 
-                    minimumValue={ -15 } 
-                    maximumValue={ 8 } 
-                    value={ outLevel } 
-                    onValueChange={()=>console.log("TODO: Change output level")} 
-                />
-            <Button title="Apply Changes" onPress={()=>console.log("TODO: Apply changes")} />
-            </View>
+  return (
+    <View style={ styles.container }>
+      <Text style={ styles.baseText }>Ch: { item.index }, Label: </Text>
+      <TextInput
+        value={ item.label }
+        style={[styles.textInput, { textAlign: "center" }]}
+      />
+      <Text style={styles.baseText}>Block: {item.block}</Text>
+      <Text style={styles.baseText}>Voltage: {item.voltage} V</Text>
 
-        </View>
-    )
-}
+      <View style={{ flex: 1, flexDirection: "row" }}>
+        <Picker // Frequency picker component
+          selectedValue={frequency}
+          style={{ height: 50, flex: 1 }}
+          onValueChange={( newFreq ) => changeFrequency(newFreq)}
+        >
+          {freqs.map(freq => (
+            <Picker.Item
+              key={ freq }
+              label={ freq.toFixed(2) + " MHz"}
+              value={ freq }
+            />
+          ))}
+        </Picker>
 
-export default DataDetails
+        <Picker //Battery type picker component
+          selectedValue={batteryType}
+          style={{ height: 50, flex: 1 }}
+          onValueChange={(value) => changeBattery(value)}
+        >
+          {Object.keys(BatteryTypes).map(key => (
+            <Picker.Item key={key} label={BatteryTypes[key]} value={key} />
+          ))}
+        </Picker>
+      </View>
+
+      <View style={styles.container}>
+        <Text>Channel Output Level: {outLevel} </Text>
+        <Slider //Channel output level slider
+          width={200}
+          minimumValue={-15}
+          maximumValue={8}
+          value={outLevel}
+          step={1}
+          onSlidingComplete={(value) => changeLevel(value)}
+        />
+        <Button
+          title="Apply Changes"
+          onPress={() => console.log("TODO: Apply changes")}
+        />
+      </View>
+    </View>
+  );
+};
+
+DataDetails.propTypes = {
+  item: PropTypes.shape({
+    frequency: PropTypes.number.isRequired,
+    block: PropTypes.string.isRequired,
+    voltage: PropTypes.number.isRequired,
+    batteryType: PropTypes.number.isRequired,
+    outLevel: PropTypes.number.isRequired
+  })
+};
+
+export default DataDetails;
--- src/components/DetailListItem.js
+++ src/components/DetailListItem.js
@@ -1,26 +1,28 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { View, Text, TouchableOpacity} from 'react-native'
-import styles from '../stylesheets/appStyles';
+import React from "react";
+import PropTypes from "prop-types";
+import { View, Text, TouchableOpacity } from "react-native";
+import styles from "../stylesheets/appStyles";
 
-const DetailListItem = (props) => {
-    return (
-        <TouchableOpacity 
-        onPress={()=>props.onBlockPress(props.item)}
-        style={styles.celledListItem} >
-                <Text>{props.item.label || "No Label"}</Text>
-                <Text>{props.item.frequency.toFixed(2)}</Text>
-                <Text>{props.item.block}</Text>
-                <Text>{props.item.voltage.toFixed(2)}</Text>
-        </TouchableOpacity>
-    )
-}
+const DetailListItem = props => {
+  return (
+    <TouchableOpacity
+      onPress={() => props.onBlockPress(props.item)}
+      style={styles.celledListItem}
+    >
+      <Text>{props.item.label || "No Label"}</Text>
+      <Text>{props.item.frequency.toFixed(2)}</Text>
+      <Text>{props.item.block}</Text>
+      <Text>{props.item.voltage.toFixed(2)}</Text>
+    </TouchableOpacity>
+  );
+};
 DetailListItem.propTypes = {
-    item : PropTypes.shape({
-        frequency: PropTypes.number.isRequired,
-        pilot: PropTypes.string.isRequired,
-        voltage: PropTypes.number.isRequired,
-        index: PropTypes.number.isRequired
-    })
-}
-export default DetailListItem
+  item: PropTypes.shape({
+    block: PropTypes.string,
+    frequency: PropTypes.number.isRequired,
+    pilot: PropTypes.string.isRequired,
+    voltage: PropTypes.number.isRequired,
+    index: PropTypes.number.isRequired
+  })
+};
+export default DetailListItem;
