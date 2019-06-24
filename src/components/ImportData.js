7566e85d55a931f6064ce352fefb54324f09bf59
Dana Simmons <dsanalog@gmail.com>
2019-06-24 15:26:01 -0400

    Extract FFX Parser from View
    Removed parsing logic for FFX files and placed them in libFFX.
    This should allow the file format to change without much effor.

--- src/components/ImportData.js
+++ src/components/ImportData.js
@@ -10,41 +10,19 @@ import React, { useState, useEffect } from 'react'
 import { View, Text } from 'react-native'
 import alertError from './alertError'
 
-const ENCODING = 'utf8' //TODO: abstract this and other constants into constants file
 
-const FileSystem = require('react-native-fs')
 
-const {parseString} = require('xml2js')
 
-async function openFile(url){
-    
-    const fileStat = await FileSystem.stat(url)
-   
-    if(fileStat.isFile()){
-        return FileSystem.readFile(fileStat.path,ENCODING)
-    }else{
-        console.log("Failed to open url:",url)
-        throw new Error("File Error: url is not a valid file")
-    }
-}
+
 
 const ImportData = (props) => {
 
     const url = props.navigation.getParam("url")
     const [txList, setTxList] = useState(null)
     
-    if( !txList ){
-        openFile(url)
-        .then(( xmlResult ) => {
-            parseString(xmlResult,(err,result)=>{
-                console.log('result', result)
-                setTxList(result.FFX.TX)
-            })
-        },(error) => alertError(error))
-    }
 
 
-    console.log(txList)
+    console.log(url)
     return (
         <View>
             <Text>Lets open some data:</Text>
