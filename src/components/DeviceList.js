b784edd892e99181a95a8ca100e2b79b0480bab6
Dana Simmons <dsanalog@gmail.com>
2019-06-30 12:40:28 -0400

    Apply prettier
    applied prettier to NewDevice and DeviceLIst

--- src/components/DeviceList.js
+++ src/components/DeviceList.js
@@ -20,31 +20,38 @@ export default class DeviceList extends Component {
     isEditing: false,
     venues: []
   };
-
+  
   _addNewVenue = venue => {
     console.log(this.state);
     const venues = this.state.venues || []; //Handle empty state with empty array
-
     venue.key = uuid();
-
     this.setState({ venues: [venue, ...venues] }, this._storeData);
   };
+
   _onPressItem = deviceData => {
     const device = connectDevice(deviceData);
     this.props.navigation.push("Device", { device });
   };
-  _removeDevice = (id) =>{
-    const { venues } = this.state
-    this.setState({venues: venues.filter(({ key }) => key !== id )})
-  }
+
+  _removeDevice = id => {
+    const { venues } = this.state;
+    this.setState({ venues: venues.filter(({ key }) => key !== id) });
+  };
+  
+  _toggleEdit = () => {
+    console.log("this.state.isEditing :", this.state.isEditing);
+    this.setState({ isEditing: !this.state.isEditing });
+  };
+
   _renderItem = ({ item }) => (
-    <DeviceListItem 
-    device={item} 
-    onPressItem={this._onPressItem} 
-    removeDevice={this._removeDevice}
-    editing={this.state.isEditing}
+    <DeviceListItem
+      device={item}
+      onPressItem={this._onPressItem}
+      removeDevice={this._removeDevice}
+      editing={this.state.isEditing}
     />
   );
+
   _storeData = async () => {
     try {
       await AsyncStorage.setItem(
@@ -57,6 +64,7 @@ export default class DeviceList extends Component {
       ]);
     }
   };
+
   _readData = async () => {
     try {
       const venues = await AsyncStorage.getItem("@PocketVenue:devices");
@@ -67,16 +75,13 @@ export default class DeviceList extends Component {
       ]);
     }
   };
-  _toggleEdit = () =>{
-    console.log('this.state.isEditing :', this.state.isEditing);
-    this.setState({isEditing: !this.state.isEditing })
-  }
+  
   render() {
     return (
       <View style={{ flex: 1 }}>
         <View style={styles.toolbar}>
           <NewDevice addNewVenue={data => this._addNewVenue(data)} />
-          <Button title={"Edit"} onPress={()=> this._toggleEdit()} />
+          <Button title={"Edit"} onPress={() => this._toggleEdit()} />
         </View>
 
         <FlatList
@@ -88,9 +93,10 @@ export default class DeviceList extends Component {
     );
   }
   _openUrl({ url }) {
-    console.log("Event:", url)
+    console.log("Event:", url);
     this.props.navigation.push("ImportData", { url });
   }
+
   componentDidMount() {
     this._readData();
     Linking.addEventListener("url", e => this._openUrl(e));
--- src/components/NewDevice.js
+++ src/components/NewDevice.js
@@ -1,95 +1,104 @@
-import React, { Component } from 'react'
-import {View, Modal, Text, Picker,Button} from 'react-native'
-import { TextInput } from 'react-native-gesture-handler';
-import styles from '../stylesheets/appStyles'
-import devices from '../lib/deviceTypes';
+import React, { Component } from "react";
+import { View, Modal, Text, Picker, Button } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import styles from "../stylesheets/appStyles";
+import devices from "../lib/deviceTypes";
 export default class NewDevice extends Component {
-    constructor(props){
-        super(props)
-        this.state ={
-            venue:{
-                address: "127.0.0.1",
-                port: "4080",
-                name: "New Venue",
-                type: devices.VRWB
-            },
-            modalVisible: false
-        }
-        
-    }
-    setModalVisible(visible) {
-        this.setState({modalVisible: visible});
-      }
-    setVeneuState(data){
-        this.setState(({venue})=>{
-            const venueData = Object.assign(venue,data)
-            return {venue:venueData}
-        },()=>console.log("State:", this.state))
-        
-    }
-    render() {      
-        const {name,port,address} = this.state.venue 
-        return (
-            <View>
-                <Modal
-                animationType="slide"
-                transparent={false}
-                visible={this.state.modalVisible}
-                onRequestClose={() => {
-                    Alert.alert('Modal has been closed.');
-                }}>
-                
-                    <View style={styles.modal}>
-                        <Text>Device Label</Text>
-                        <TextInput 
-                            style={styles.textInput}
-                            onChangeText={(text)=>this.setVeneuState({name:text})}   
-                            value={name}/> 
+  constructor(props) {
+    super(props);
+    this.state = {
+      venue: {
+        address: "127.0.0.1",
+        port: "4080",
+        name: "New Venue",
+        type: devices.VRWB,
+      },
+      modalVisible: false
+    };
+  }
 
-                        <Text>Device Address</Text>
-                        <TextInput 
-                            style={styles.textInput}
-                            onChangeText={(text)=>this.setVeneuState({address:text})} 
-                            value={address}/>
-                            
-                        <Text>Device Port</Text>
-                        <TextInput 
-                            style={styles.textInput}
-                            onChangeText={(text)=>this.setVeneuState({port:text})} 
-                            value={port}
-                        />
+  setModalVisible(visible) {
+    this.setState({ modalVisible: visible });
+  }
 
-                        <Text>Device Type</Text>
-                        <Picker style={{flex:1}}
-                            selectedValue={this.state.venue.type}
-                            onValueChange={( value ) => this.setVeneuState({ type: value })}
-                        >
-                            {
-                                Object.values(devices).map(( value ) => <Picker.Item key={ value } value={ value } label={ value }/> )
-                            }
-                        </Picker>
+  setVeneuState(data) {
+    this.setState(
+      ({ venue }) => {
+        const venueData = Object.assign(venue, data);
+        return { venue: venueData };
+      },
+      () => console.log("State:", this.state)
+    );
+  }
+  render() {
+    const { name, port, address } = this.state.venue;
+    return (
+      <View>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+          }}
+        >
+          <View style={styles.modal}>
+            <Text>Device Label</Text>
+            <TextInput
+              style={styles.textInput}
+              onChangeText={text => this.setVeneuState({ name: text })}
+              value={name}
+            />
 
-                        <Button
-                        title={"Add"}
-                        onPress={() => {
-                            this.props.addNewVenue(this.state.venue)
-                            this.setModalVisible(!this.state.modalVisible);
-                        }} />
+            <Text>Device Address</Text>
+            <TextInput
+              style={styles.textInput}
+              onChangeText={text => this.setVeneuState({ address: text })}
+              value={address}
+            />
 
-                        <Button
-                        title={"Cancel"}
-                        onPress={()=> {
-                            this.setModalVisible(!this.state.modalVisible)
-                        }} />
-                    </View>
-                </Modal>
-        
-                <Button
-                title={"New Venue"}
-                onPress={() => {
-                    this.setModalVisible(true);
-                }} />
+            <Text>Device Port</Text>
+            <TextInput
+              style={styles.textInput}
+              onChangeText={text => this.setVeneuState({ port: text })}
+              value={port}
+            />
+
+            <Text>Device Type</Text>
+            <Picker
+              style={{ flex: 1 }}
+              selectedValue={this.state.venue.type}
+              onValueChange={value => this.setVeneuState({ type: value })}
+            >
+              {Object.values(devices).map(value => (
+                <Picker.Item key={value} value={value} label={value} />
+              ))}
+            </Picker>
+
+            <Button
+              title={"Add"}
+              onPress={() => {
+                this.props.addNewVenue(this.state.venue);
+                this.setModalVisible(!this.state.modalVisible);
+              }}
+            />
+
+            <Button
+              title={"Cancel"}
+              onPress={() => {
+                this.setModalVisible(!this.state.modalVisible);
+              }}
+            />
           </View>
-        )
-    }
+        </Modal>
+
+        <Button
+          title={"New Venue"}
+          onPress={() => {
+            this.setModalVisible(true);
+          }}
+        />
+      </View>
+    );
+  }
 }
