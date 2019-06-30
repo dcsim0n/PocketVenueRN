dbd63597e41fa22ca62f8539e4d9d9b009ed0b10
Dana Simmons <dsanalog@gmail.com>
2019-06-30 18:07:10 -0400

    Add animated compoents
    begining to implement some animations, not quite there yet.

--- src/components/DeviceListItem.js
+++ src/components/DeviceListItem.js
@@ -1,20 +1,40 @@
-import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
-import { Icon, ListItem } from "native-base";
+import React, { useState, useEffect, useLayoutEffect } from "react";
+import { View, Text, Animated } from "react-native";
+import { Icon, ListItem, Body, Right } from "native-base";
 import styles from "../stylesheets/appStyles";
 
 const DeviceListItem = props => {
   const { device } = props;
+  const [opacity, setOpacity] = useState(new Animated.Value(0))
+  useEffect(() => {
+    if(props.editing && opacity === 0){
+        Animated.timing(opacity,{
+            duration: 1000,
+            toValue: 1
+        }).start();
+        
+    }else{
+        Animated.timing(opacity,{
+            duration: 1000,
+            toValue: 0
+        }).start() 
+    }
+  }, [props.editing])
   return (
-    <ListItem button onPress={() => props.onPressItem(device)}>
-        <Text>{`${device.name} @ ${device.address} : ${
-          device.port
-        }`}</Text>
-      <Icon
-        type="Feather"
-        name="trash-2"
-        onPress={() => props.removeDevice(device.key)}
-      />
+    <ListItem icon button onPress={() => props.onPressItem(device)}>
+      <Body>
+        <Text>{`${device.name} @ ${device.address} : ${device.port}`}</Text>
+      </Body>
+      <Right>
+        <Animated.View style={{ opacity: opacity}}>
+            <Icon
+            type="Feather"
+            name="trash-2"
+            onPress={() => props.removeDevice(device.key)}
+            style={{ color: 'red' }}
+            />
+        </Animated.View>
+      </Right>
     </ListItem>
   );
 };
