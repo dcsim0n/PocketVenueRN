cebb9be5831a262d334ebb1052c68859d5ba7fec
Dana Simmons <dsanalog@gmail.com>
2019-06-30 18:10:38 -0400

    Add remove functionality
    added a trash can icon for removing devices from the device list.
    The icon is attached to the removeDevice method in DeviceList.

--- src/components/DeviceListItem.js
+++ src/components/DeviceListItem.js
@@ -1,20 +1,26 @@
-import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
-import { Icon, ListItem } from "native-base";
+import React, { useState, useEffect, useLayoutEffect } from "react";
+import { View, Text, Animated } from "react-native";
+import { Icon, ListItem, Body, Right } from "native-base";
 import styles from "../stylesheets/appStyles";
 
 const DeviceListItem = props => {
   const { device } = props;
+  const opacity = new Animated.Value(1);
   return (
-    <ListItem button onPress={() => props.onPressItem(device)}>
-        <Text>{`${device.name} @ ${device.address} : ${
-          device.port
-        }`}</Text>
-      <Icon
-        type="Feather"
-        name="trash-2"
-        onPress={() => props.removeDevice(device.key)}
-      />
+    <ListItem icon button onPress={() => props.onPressItem(device)}>
+      <Body>
+        <Text>{`${device.name} @ ${device.address} : ${device.port}`}</Text>
+      </Body>
+      <Right>
+        <Animated.View style={{ opacity: opacity }}>
+          <Icon
+            type="Feather"
+            name="trash-2"
+            onPress={() => props.removeDevice(device.key)}
+            style={{ color: "red" }}
+          />
+        </Animated.View>
+      </Right>
     </ListItem>
   );
 };
