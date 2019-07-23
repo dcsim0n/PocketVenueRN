5b83f860c31c79233aad515ccc7d4bf00d02da7d
Dana Simmons <dsanalog@gmail.com>
2019-07-23 12:51:49 -0400

    Add navigateWithDevice method
    added navigateWithDevice method to withDevice HOC.
    This handles passing the necessary params to the navigator for any
    component that needs to work with a device

--- src/components/DeviceDetails.js
+++ src/components/DeviceDetails.js
@@ -1,44 +1,51 @@
-import React, {Component} from 'react'
-import uuid from 'uuid/v1'
-import PropTypes from 'prop-types'
-import { View, Text, Button, FlatList} from 'react-native'
-import {withNavigationFocus} from 'react-navigation';
-import {withDevice} from '../lib/withDevice'
-import DetailListItem from './DetailListItem'
-import styles from '../stylesheets/appStyles'
+import React, { Component } from "react";
+import uuid from "uuid/v1";
+import PropTypes from "prop-types";
+import { View, Text, Button, FlatList } from "react-native";
+import { withDevice } from "../lib/withDevice";
+import DetailListItem from "./DetailListItem";
+import styles from "../stylesheets/appStyles";
 
 class DeviceDetails extends Component {
-
-    _onBlockPress = (item)=>{
-        const {device} = this.props
-        this.props.navigation.push("Data",{item,device})
-    }
-    render(){
-        return(
-            <View style={styles.container}>
-                <View style={styles.container}>
-                    <Text>{`${this.props.device.name} @ ${this.props.device.address}: ${this.props.device.type}`}</Text>
-                    <FlatList
-                    contentContainerStyle={styles.celledList}
-                    numColumns={2}
-                    horizontal={false}
-                    data={this.props.deviceData}
-                    keyExtractor={()=>uuid()}
-                    renderItem={({item})=>(
-                        <DetailListItem onBlockPress={this._onBlockPress} item={item} />
-                    )} />
-                </View>
-                <View style={[styles.toolbar, {alignContent:'flex-end'}]}>
-                    <Button title={"Scan"} onPress={()=>this.props.navigation.push("Scan",{device:this.props.device})}/>
-                    <Button title={"Device Settings"} />
-                </View>
-            </View>
-        )
-    }
+  _onBlockPress = item => {
+    const { device } = this.props;
+    this.props.navigation.push("Data", { item, device });
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.container}>
+          <Text>{`${this.props.device.name} @ ${this.props.device.address}: ${
+            this.props.device.type
+          }`}</Text>
+          <FlatList
+            contentContainerStyle={styles.celledList}
+            numColumns={2}
+            horizontal={false}
+            data={this.props.deviceData}
+            keyExtractor={() => uuid()}
+            renderItem={({ item }) => (
+              <DetailListItem onBlockPress={this._onBlockPress} item={item} />
+            )}
+          />
+        </View>
+        <View style={[styles.toolbar, { alignContent: "flex-end" }]}>
+          <Button
+            title={"Scan"}
+            onPress={() => this.props.navigateWithDevice("Scan")}
+          />
+          <Button
+            title={"Device Settings"}
+            onPress={() => this.props.navigateWithDevice("DeviceSettings")}
+          />
+        </View>
+      </View>
+    );
+  }
 }
 DeviceDetails.propTypes = {
-    deviceData: PropTypes.array,
-    scanData: PropTypes.array,
-    device: PropTypes.object
-}
-export default withDevice(DeviceDetails)
+  deviceData: PropTypes.array,
+  scanData: PropTypes.array,
+  device: PropTypes.object
+};
+export default withDevice(DeviceDetails);
--- src/lib/withDevice.js
+++ src/lib/withDevice.js
@@ -1,67 +1,75 @@
-import React, { Component } from 'react'
-import {Alert} from 'react-native'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import { Alert } from "react-native";
+import PropTypes from "prop-types";
 
-
-
-export function withDevice(ComponentToWrap){
-
-    class Wrapper extends Component {
-        constructor(props) {
-            super(props)
-            this.navigation = this.props.navigation
-            this.device = this.navigation.getParam('device')
-            this.blurListener = this.navigation.addListener('willBlur',this.componentWillBlur)
-            this.focusListener = this.navigation.addListener('didFocus',this.componentDidFocus)
-            this.state = {
-                 deviceData: [],
-                 scanData: [],
-                 refreshRate: 2000
-            }
-        }
-        handleDeviceData = ()=>{
-            this.setState({deviceData: this.device.deviceData})
-        }
-        handleScanData = ()=>{
-            this.setState({scanData: this.device.scanData})
-        }
-        handleError = (error)=>{
-            this.device.stop()
-            Alert.alert(
-                "Device Error",
-                `${error.name}: ${error.message}`,
-                [{text:'OK'}]
-            )
-        }
-        componentDidFocus = ()=>{
-            console.log('focusing')
-            this.device.start(
-                this.state.refreshRate,
-                this.handleDeviceData,
-                this.handleError)
-        }
-        componentWillBlur = ()=>{
-            console.log('blurring..')
-            this.device.stop()
-        }
-        componentWillUnmount(){
-            this.blurListener.remove()
-            this.focusListener.remove()
-        }
-        render() {
-            return (
-                <ComponentToWrap
-                deviceData={this.state.deviceData} 
-                scanData={this.state.scanData}
-                device={this.device} 
-                {...this.props}/>
-            )
-        }
+export function withDevice(ComponentToWrap) {
+  class Wrapper extends Component {
+    constructor(props) {
+      super(props);
+      this.navigation = this.props.navigation;
+      this.device = this.navigation.getParam("device");
+      this.blurListener = this.navigation.addListener(
+        "willBlur",
+        this.componentWillBlur
+      );
+      this.focusListener = this.navigation.addListener(
+        "didFocus",
+        this.componentDidFocus
+      );
+      this.state = {
+        deviceData: [],
+        scanData: [],
+        refreshRate: 2000
+        //TODO: HEY! This is where the global settings can live,
+        // but how can we share the state between components?
+      };
     }
-    Wrapper.propTypes = {
-        navigation: PropTypes.object.isRequired
+    handleDeviceData = () => {
+      this.setState({ deviceData: this.device.deviceData });
+    };
+    handleScanData = () => {
+      this.setState({ scanData: this.device.scanData });
+    };
+    navigateWithDevice = screenName => {
+      this.navigation.push(screenName, { device: this.device });
+    };
+    handleError = error => {
+      this.device.stop();
+      Alert.alert("Device Error", `${error.name}: ${error.message}`, [
+        { text: "OK" }
+      ]);
+    };
+    componentDidFocus = () => {
+      console.log("focusing");
+      this.device.start(
+        this.state.refreshRate,
+        this.handleDeviceData,
+        this.handleError
+      );
+    };
+    componentWillBlur = () => {
+      console.log("blurring..");
+      this.device.stop();
+    };
+    componentWillUnmount() {
+      this.blurListener.remove();
+      this.focusListener.remove();
     }
-    
-    return Wrapper
+    render() {
+      return (
+        <ComponentToWrap
+          deviceData={this.state.deviceData}
+          scanData={this.state.scanData}
+          device={this.device}
+          navigateWithDevice={this.navigateWithDevice}
+          {...this.props}
+        />
+      );
+    }
+  }
+  Wrapper.propTypes = {
+    navigation: PropTypes.object.isRequired
+  };
+
+  return Wrapper;
 }
-    
