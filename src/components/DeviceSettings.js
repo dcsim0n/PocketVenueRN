46cd182abd205c95345c2e1f9156588e552c2371
Dana Simmons <dsanalog@gmail.com>
2019-08-18 12:34:04 -0400

    Convert preferences to hash map
    converting preferences to a hash map allows direct lookup and
    preserves the device key. Currently using the battery 'type' as a hash
    key, which is an integer, could probably change that to be the
    "battery type" name ie "Alkaine AA" instead of "0"

--- src/components/DeviceSettings.js
+++ src/components/DeviceSettings.js
@@ -5,56 +5,65 @@
 |--------------------------------------------------
 */
 
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-import { withDevice } from '../lib/withDevice';
-import Slider from '@react-native-community/slider';
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+import { withDevice } from "../lib/withDevice";
+import Slider from "@react-native-community/slider";
 
-
- class DeviceSettings extends Component {
-
-  handleSlide( {setting, value, type} ){
+class DeviceSettings extends Component {
+  //TODO: add local state values to improve slider performance,
+  // only dispatch redux action on onSlideComplete.
+  handleSlide({ key, value, type }) { // <-- here type is the "type" of value: "warn", "alert", etc.. 
+                                      // key is the "prefernce key"
     const newPreference = Object.assign(
+      // Merge new value into old preference
       {},
-      setting,
-      {[type]: value }
-      );
-      console.log("new preference is: ", newPreference)
-      // make new preference array
-      // insert newPreference into new preferece array using newPreference.index
-      // dispatch newPreferences.
-    const newPreferenceList = this.props.preferences.slice() //copy preferences
-    newPreferenceList.key = this.props.preferences.key // keep track of the associated devcie key
-    newPreferenceList[setting.index] = newPreference  
-    this.props.addSetting( newPreferenceList );
+      this.props.preferences[key],
+      { [type]: value }
+    );
+
+    const newPreferences = Object.assign(
+      // Merge new preference into old preference(s)
+      {},
+      this.props.preferences,
+      { [key]: newPreference }
+    );
+
+    console.log("Created new preferences:", newPreferences);
+    this.props.addSetting(newPreferences);
   }
+
   render() {
     return (
       <View>
-        { this.props.preferences.map(( setting ) => (
-          <View key={ setting.preferenceKey }>
-            <Text>{setting.type}</Text>
+        {Object.keys(this.props.preferences).map(key => ( // <-- here key is the battery type "0", "1", etc..
+          <View key={key}>
+            <Text>{this.props.preferences[key].name}</Text>
             <Text>Warn Level</Text>
-            <Slider  
-             minimumValue={0} 
-             maximumValue={9} 
-             value={setting.warn} 
-             onValueChange={( value ) => this.handleSlide( { setting, value, type:'warn' } )} 
+            <Slider
+              minimumValue={0}
+              maximumValue={9}
+              value={this.props.preferences[key].warn}
+              onValueChange={value =>
+                this.handleSlide({ key, value, type: "warn" })
+              }
             />
             <Text>Alert Level</Text>
-            <Slider  
-             minimumValue={0} 
-             maximumValue={9} 
-             value={setting.alert}
-             onValueChange={( value ) => this.handleSlide( { setting, value, type:'alert' } )} 
-             />
+            <Slider
+              minimumValue={0}
+              maximumValue={9}
+              value={this.props.preferences[key].alert}
+              onValueChange={value =>
+                this.handleSlide({ key, value, type: "alert" })
+              }
+            />
           </View>
         ))}
       </View>
-    )
+    );
   }
 }
-export default withDevice(DeviceSettings)
+export default withDevice(DeviceSettings);
 /*
 Device Structure
 {
@@ -62,21 +71,19 @@ Device Structure
   address: "192.168.1.1",
   port: "4080",
   type: "VRM2WB"
-  settings: {
-    battery: {
-      0: { //battery types from device
-        warnLevel: 12, // red ?
-        alertLevel: 14 // yellow ?
-      },
-      1: {
-        {
-          warnLevel: 13,
-          alertLevel: 15
-        }
+  preferences: {
+    0: { //battery types from device
+      warnLevel: 12, // red ?
+      alertLevel: 14 // yellow ?
+    },
+    1: {
+      {
+        warnLevel: 13,
+        alertLevel: 15
       }
-    }
-    refresh: 12 //ms between updates
-    scanRate: 12 //ms between scan updates
+    },
+    refresh: 12, //ms between updates
+    scanRate: 12, //ms between scan updates
   }
 }
 */
--- src/lib/deviceTypes.js
+++ src/lib/deviceTypes.js
@@ -1,74 +1,68 @@
-import VRM2WB from './libVRM2WB';
-import Dummy from './libDummy';
-import VRWB from './libVRWB';
-import M2T from './libM2T'
+import VRM2WB from "./libVRM2WB";
+import Dummy from "./libDummy";
+import VRWB from "./libVRWB";
+import M2T from "./libM2T";
 
-function buildBatterySettings( batteryTypes ){
-    const settings = Object.keys(batteryTypes).map(( key, i ) => {
-        // create a 'Warn' and a 'Alert' setting for each type of battery
-        return { preferenceKey: `${batteryTypes[key]}`,  alert: 0, warn: 0 , index: i }
-    })
-    return settings 
+const DEFAULT_WARN = 0;
+const DEFAULT_ALERT = 0;
+function buildBatterySettings(batteryTypes) {
+  const preferences = {};
+  for (k in batteryTypes) {
+    preferences[k] = { name: batteryTypes[k] , alert: DEFAULT_ALERT, warn: DEFAULT_WARN };
+  }
+  return preferences;
 }
-export default DeviceTypes = {
-    VRM2WB: {
-        name: 'Venue 2 / VRM2WB',
-        initialize: ( options ) =>{
-            const device = new VRM2WB( options )
-            let preferences = options.preferences
-            if( !preferences ){
-                preferences = buildBatterySettings(device.BatteryTypes)
-                // preferences is an array object, 
-                // not sure how I feel about adding a key to it
-                // vvvvvvvv    
-                preferences.key = options.key
-            }
-            return { device, preferences }
-        }
-    },
-    VRWB: {
-        name: 'Venue WB / VRWB',
-        initialize: ( options ) =>{
-            const device = new VRWB( options )
-            let preferences = options.preferences
-            if( !preferences ){
-                preferences = buildBatterySettings(device.BatteryTypes)
-                // preferences is an array object, 
-                // not sure how I feel about adding a key to it
-                // vvvvvvvv    
-                preferences.key = options.key
-            }
-            return { device, preferences }
-        }
-    } ,
-    // M2T: {
-    //     name:  "M2 Duet",
-    //     initialize: ( options ) =>{
-    //         const device = new M2T( options )
-    //         const settings = buildBatterySettings(device.BatteryTypes)
-    //         return { device, settings }
-    //     }
-    // },
-    // DSQD: {
-    //     name: "D\xB2 Reciever",
-    //     initialize: ( options ) =>{
-    //         console.log("TODO: Build D2 support")
-    //         throw Error("D2 Not implemented yet")
-    //     }
-    // },
-    TEST: {
-        name: "Demo Mode",
-        initialize: ( options ) =>{
-            const device = new Dummy( options )
-            let preferences = options.preferences
-            if( !preferences ){
-                preferences = buildBatterySettings(device.BatteryTypes)
-                // preferences is an array object, 
-                // not sure how I feel about adding a key to it
-                // vvvvvvvv    
-                preferences.key = options.key
-            }
-            return { device, preferences }
-       }
+
+export default (DeviceTypes = {
+  VRM2WB: {
+    name: "Venue 2 / VRM2WB",
+    initialize: options => {
+      const device = new VRM2WB(options);
+      let preferences = options.preferences;
+      if (!preferences) { // If preferences is undefined, build them from scratch
+        preferences = buildBatterySettings(device.BatteryTypes);
+        preferences.key = options.key;
+      }
+      return { device, preferences };
     }
-}
+  },
+  VRWB: {
+    name: "Venue WB / VRWB",
+    initialize: options => {
+      const device = new VRWB(options);
+      let preferences = options.preferences;
+      if (!preferences) { // If preferences is undefined, build them from scratch
+        preferences = buildBatterySettings(device.BatteryTypes);
+        preferences.key = options.key;
+      }
+      return { device, preferences };
+    }
+  },
+  // M2T: {
+  //     name:  "M2 Duet",
+  //     initialize: ( options ) =>{
+  //         const device = new M2T( options )
+  //         const settings = buildBatterySettings(device.BatteryTypes)
+  //         return { device, settings }
+  //     }
+  // },
+  // DSQD: {
+  //     name: "D\xB2 Reciever",
+  //     initialize: ( options ) =>{
+  //         console.log("TODO: Build D2 support")
+  //         throw Error("D2 Not implemented yet")
+  //     }
+  // },
+  TEST: {
+    name: "Demo Mode",
+    initialize: options => {
+      const device = new Dummy(options);
+      let preferences = options.preferences;
+      if (!preferences) { // If preferences is undefined, build them from scratch
+        preferences = buildBatterySettings(device.BatteryTypes);
+        preferences.key = options.key;
+      }
+      return { device, preferences };
+    }
+  }
+});
