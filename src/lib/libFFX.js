63195e15dadf35a3a50bfb47bb7b386b54f911ba
Dana Simmons <dsanalog@gmail.com>
2020-06-06 00:50:24 -0400

    Clean and simplify the libFFX library``
    Why: I think I wrote this code when I was just learning about
    jest and how promises really worked. Since learned that throwing
    errors inside of a promise won't necessarily cause the promise to
    reject, need the Promise.reject() method for that
    
    *---
    
    How: Removed the unnecessary function wrappers. Added proper Promise.reject() return
    values for error conditions.
    Upgraded xml2js to .4.23 and adjusted the parsing parameters
    
    *---

--- src/lib/libFFX.js
+++ src/lib/libFFX.js
@@ -5,53 +5,38 @@
 */
 const ENCODING = "utf8"; //TODO: abstract this and other constants into constants file
 
-const DEBUG = false;
+const DEBUG = true;
 
 const FileSystem = require("react-native-fs");
 
-const { Builder, parseString } = require("xml2js");
+const { Builder, parseStringPromise } = require("xml2js");
+
 
 export function ParseFFX(url) {
-  return new Promise((resolve, reject) => {
-    _ParseFFX(resolve, reject, url);
-  });
-}
-function _ParseFFX(resolve, reject, url) {
+  // return new Promise((resolve, reject) => {
+  //   _ParseFFX(resolve, reject, url);
+  // });
   //Open File from url if it is valid
   //Check if it is an xml/ffx format
   //if it is parse it into an JS Object
   //return promise for this data
-  try {
-    const decodedURL = decodeURI(url);
-    DEBUG && console.log("Opening URL: ", decodedURL);
-    if (!decodedURL.includes("ffx")) {
-      throw new Error(
-        "File Error: file does not end with a valid 'ffx' extension"
-      );
-    }
-
-    openFile(decodedURL).then(data => {
-      parseString(
-        data,
-        { ignorAttrs: true, mergeAttrs: true, explicitArray: false },
-        (err, result) => {
-          try {
-            if (err) {
-              throw err;
-            }
-            if(result.FFX === undefined){
-              throw new Error("File Error: missing FFX property")
-            }
-            resolve(result.FFX);
-          } catch (error) {
-            reject(error);
-          }
-        }
-      );
-    });
-  } catch (error) {
-    reject(error);
+  const decodedURL = decodeURI(url);
+  DEBUG && console.log("Opening URL: ", decodedURL);
+  if (!decodedURL.includes("ffx")) {
+    return Promise.reject("File Error: file does not end with a valid 'ffx' extension");
   }
+
+  return openFile(decodedURL)
+  .then(data => {
+    return parseStringPromise(data, {mergeAttrs: true, explicitArray: false})
+    .then((result) => {
+      if(result.FFX === undefined){
+        return Promise.reject("File Error: file does not end with a valid 'ffx' extension")
+      }
+      DEBUG && console.log('result.FFX', result.FFX);
+      return result.FFX;
+    });
+  });
 }
 function _RenderFFX(txList){
   const builder = new Builder({ rootName: "FFX" })
@@ -65,7 +50,7 @@ async function openFile(url) {
     return await FileSystem.readFile(fileStat.path, ENCODING);
   } else {
     DEBUG && console.log("Failed to open url:", url);
-    throw new Error("File Error: url is not a valid file");
+    return Promise.reject("File Error: url is not a valid file");
   }
 }
 
