b572a1bd5634f42ce17cc2e413427dd123ecf2c6
Dana Simmons <dsanalog@gmail.com>
2020-06-06 00:45:15 -0400

    Update libFFX test suite
    Why: had mutliple problems race conditions and mistakes in the
    design of these tests
    
    *---
    
    How: adjusted the testing of the ParseFFX lib to use proper promise event
    handlers.
    
    Discovered that this version of jest may have a problem with returning rejected promises
    Worked around this by using the done() callback
    
    All tests passing now
    
    *---

--- __tests__/libFFX-test.js
+++ __tests__/libFFX-test.js
@@ -1,8 +1,10 @@
 import "react-native";
 
-jest.mock("react-native-fs");
 
 import { ParseFFX } from "../src/lib/libFFX";
+
+jest.mock('fs');
+
 
 describe("Use React Native FS Mock", () => {
   const fs = require("react-native-fs");
@@ -15,36 +17,57 @@ describe("Use React Native FS Mock", () => {
   test("Have a readFile property", () => {
     expect(fs.readFile).toBeDefined();
   });
+  test("Stat rejects when it should", (done) =>{
+    fs.__setStatData(false)
+    fs.stat()
+    .then( res =>{
+      expect(res.isFile()).toBe(false)
+      done()
+    })
+  })
 });
 
 describe('ParseFFX promise', () => {
-  const fs = require("react-native-fs");
+  
   test("Promise rejects without a valid file name", () => {
-    expect.assertions(1);
     return ParseFFX(null).catch(e => {
-      expect(e.name).toBe("Error");
+      expect(e).toContain("Error");
     });
   });
-  test("Checks that the url is a file", () => {
-    //expect.assertions(1)
-    ParseFFX("hello.ffx");
-    expect(fs.stat).toBeCalled();
+
+  test("Checks that the url is a file", (done) => {
+    const fs = require("react-native-fs");
+    ParseFFX("test-ffx.ffx")
+    .finally(()=>{
+      expect(fs.stat).toBeCalled();
+      done()
+    })
   });
-  test("Does not try to open the file if it's not a file",()=>{
+  test("Does not try to open the file if it's not a file",(done)=>{
+    const fs = require("react-native-fs");
     fs.__setStatData(false)
-    ParseFFX("hello.ffx");
-    expect(fs.readFile).not.toBeCalled()
+    ParseFFX("test-ffx.ffx")
+    .finally(() =>{
+      expect(fs.readFile).not.toBeCalled()
+      done()
+    })
   })
-  test("Trys to open if it is a valid file",()=>{
+  test("Trys to open if it is a valid file",(done)=>{
+    const fs = require('react-native-fs');
     fs.__setStatData(true)
-    ParseFFX("hello.ffx");
-    expect(fs.readFile).toBeCalled()
+    ParseFFX("test-ffx.ffx")
+    .finally(()=>{
+      expect(fs.readFile).toBeCalled()
+      done()
+    })
   })
   
-  test("Promise rejects if it can't open the file", () => {
-    expect.assertions(1);
-    return ParseFFX("hello.ffx").catch(e => {
-      expect(e.name).toBe("Error");
+  test("Promise rejects if it can't open the file", (done) => {
+    const fs = require('react-native-fs');
+    fs.__setTestData("");
+    return ParseFFX("test-ffx.ffx").catch(e => {
+      expect(e).toBeDefined();
+      done()
     });
   });
 })
